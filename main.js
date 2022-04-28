ef893f9ec6aeb5a829c90f2ea091c011852a3705
Andre Sales <asalesg@gmail.com>
2022-04-28 19:04:47 -0300

    att

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function carregar () {
     var msg = document.querySelector('.msg')
     var img = document.querySelector('.imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 19
+    var hora = data.getHours()
+
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >= 0 && hora < 12) {
         img.src = './fotos/manha.jpg'
